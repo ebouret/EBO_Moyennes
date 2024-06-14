48b9718f20ed0ae53942890d2dc5df5d7a0feffa
ebouret <ellybouret@gmail.com>
2024-06-14 12:18:29 +0200

    Add files via upload

---
+++ Classe.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    class Classe
+    {
+        public string nomClasse
+        { get; private set; }
+
+        public string[] matieres
+        { get; private set; }
+
+        public Eleve[] eleves
+        { get; private set; }
+
+        //indices des eleves et matieres
+        private int i = 0;
+        public int j = 0;
+
+        //Constructeur
+        public Classe(string c)
+        {
+            nomClasse = c;
+            matieres = new string[10];
+            eleves = new Eleve[30];
+        }
+
+        //Méthodes
+        public void ajouterEleve(string prenom, string nom)
+        {
+            eleves[i] = new Eleve(prenom, nom);
+            i++;
+        }
+
+        public void ajouterMatiere(string matiere)
+        {
+            matieres[j] = matiere;
+            j++;
+        }
+        
+        public decimal moyenneGeneral()
+        {
+            decimal totalG = 0;
+            int nbrMoyM = 0;
+
+            for (int l = 0; l < j; l++)
+            {
+                totalG = totalG + moyenneMatiere(l);
+                nbrMoyM++;
+            }
+
+            return Math.Round(totalG / nbrMoyM, 2);
+        }
+
+        public decimal moyenneMatiere(int m)
+        {
+            decimal totalM = 0;
+            int nbrMoy = 0;
+
+            for (int l = 0; l<i; l++)
+            {
+                totalM = totalM + eleves[l].moyenneMatiere(m);
+                nbrMoy++;
+            }
+
+            return Math.Round(totalM / nbrMoy, 2);
+        }
+
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    class Eleve
+    {
+        public string prenom
+        { get; private set; }
+        public string nom
+        { get; private set; }
+        public Note[] note
+        { get; private set; }
+        
+        //Compteur de notes
+        private int k;
+
+        //constructeur
+        public Eleve(string p, string n)
+        {
+            prenom = p;
+            nom = n;
+            note = new Note[200];
+
+        }
+
+        public decimal moyenneGeneral()
+        {
+            decimal totalG = 0;
+            int nbrMoy = 0;            
+
+            for (int l = 0; l < 10;  l++)
+            {
+                if (moyenneMatiere(l) == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    totalG = totalG + moyenneMatiere(l);
+                    nbrMoy = nbrMoy + 1;
+                }
+                
+            }
+
+            return Math.Round(totalG / nbrMoy, 2);
+        }
+
+        public decimal moyenneMatiere(int m)
+        {
+            float totalM = 0;
+            int nbrNotes = 0;
+
+            for (int l= 0; l < k; l++)
+            {
+                
+                if (note[l].matiere == m)
+                {
+                    totalM = totalM + note[l].note;
+                    nbrNotes = nbrNotes + 1;
+                }
+            }
+
+            //Test : si la matiere n'a aucune note, retourne 0
+            if (nbrNotes == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Round((decimal)totalM / nbrNotes, 2);
+            }
+                
+        }
+
+        public void ajouterNote(Note n)
+        {
+            note[k] = n;
+            k++;
+        }
+    }
+}
